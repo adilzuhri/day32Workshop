b8cc0a145a8dafb16b0644b25458f1f123a8328f
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2023-01-26 10:07:41 +0800

    completed Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'day32Workshop';
+
+  items = [
+    {name: 'Airpod', id: 1, rpice: 250 },
+    {name: 'iPad', id: 2, rpice: 650 },
+    {name: 'AirMax', id: 3, rpice: 450 }
+
+  ]
+
+  selectedItem: any;
+
+  onItemReceive (item: any) {
+    this.selectedItem = item;
+  }
+
+  // addItem(newItemEvent, string) [
+  //   this.itemList.push(newItem);]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
